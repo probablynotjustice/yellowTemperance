66076ea100eeac7bb4b6871da186e01afb70aa82
probablynotjustice <justice.77.da@gmail.com>
2026-03-01 04:29:26 -0500

    starting point

--- yellowTemperance/routes/web.php
+++ yellowTemperance/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Landing');
 })->name('home');
 
 Route::view('dashboard', 'dashboard')
