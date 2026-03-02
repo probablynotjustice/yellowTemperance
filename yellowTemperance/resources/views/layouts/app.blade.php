5e99f343b9cdd0bb7c61bc63a3592e6f9a8ad97a
probablynotjustice <justice.77.da@gmail.com>
2026-03-02 02:12:55 -0500

    Footer added

--- yellowTemperance/resources/views/layouts/app.blade.php
+++ yellowTemperance/resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
-<x-layouts::app.sidebar :title="$title ?? null">
+<x-layouts::app.sidebar :title="$title ?? null" class="min-h-screen">
     <flux:main>
         {{ $slot }}
     </flux:main>
+    @include('partials.footer')
 </x-layouts::app.sidebar>
---
+++ yellowTemperance/resources/views/partials/footer.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
+
+<footer class="py-12 border-t border-white/10 bg-black">
+        <div class="container mx-auto px-4 text-center">
+            <p class="text-gray-500">Blank Footer.</p>
+            <p>&copy; {{ date('Y') }} My App. All rights reserved.</p>
+        </div>
+    </footer>
+
+</html>
