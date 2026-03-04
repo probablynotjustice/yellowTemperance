ba2ee493187086cca768fd4ae43bd8157df516c9
probablynotjustice <justice.77.da@gmail.com>
2026-03-04 18:01:24 -0500

    backtracking through lost thoughts 2

---
+++ yellowTemperance/resources/views/admin/products/index.blade.php
@@ -0,0 +1,3 @@
+<!DOCTYPE html>
+<h1>PRODUCTS</h1>
+<a href="/admin/products/create">Create Product</a>
--- yellowTemperance/routes/web.php
+++ yellowTemperance/routes/web.php
@@ -10,4 +10,11 @@ Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+
+Route::prefix('admin')->group(funtion (){
+
+    Route::get('/', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+})    
 require __DIR__.'/settings.php';
