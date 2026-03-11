7522b600578fccc4846b6a4a6d56da97a86561c3
probablynotjustice <justice.77.da@gmail.com>
2026-03-11 16:15:57 -0400

    forgot to push: making the admin for the database seeder

--- yellowTemperance/database/seeders/DatabaseSeeder.php
+++ yellowTemperance/database/seeders/DatabaseSeeder.php
@@ -20,6 +20,12 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('password'),
+            'is_admin' => true,
+        ]);
       Product::factory()->count(10)->create(); 
     }
 }
