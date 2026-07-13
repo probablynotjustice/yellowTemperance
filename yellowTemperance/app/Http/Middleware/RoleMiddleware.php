58d44ffe1fa89c5bf5f962560432ea05b81ceec3
probablynotjustice <justice.77.da@gmail.com>
2026-07-12 23:11:33 -0500

    im having trupbe with middleware and admin routing

---
+++ yellowTemperance/app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class RoleMiddleware
+{
+    public function handle(Request $request, Closure $next, string $role)
+    {
+        if (! auth()->check()) {
+            abort(403);
+        }
+
+        if (! auth()->user()->roles->contains('name', $role)) {
+            abort(403);
+        }
+
+        return $next($request);
+    }
+}
