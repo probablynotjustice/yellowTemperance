00ca2fa9f393ca750fe34267c3140a145fc50e6d
probablynotjustice <justice.77.da@gmail.com>
2026-07-14 02:02:59 -0500

    auction seeder and Factory in progress

--- yellowTemperance/database/factories/CategoryFactory.php
+++ yellowTemperance/database/factories/CategoryFactory.php
@@ -16,8 +16,24 @@ class CategoryFactory extends Factory
      */
     public function definition(): array
     {
+        $startingBid = fake()->numberBetween(5, 100);
+        $reservePrice = fake()->numberBetween(
+            $startingBid,
+            $startingBid * 10
+        );
+        $startsAt = fake()->dateTimeBetween('-2 days', '+2 days');
+        $endsAt = fake()->dateTimeBetween(
+            $startsAt,
+            '+14 days'
+        );
         return [
-            //
+            'ticket_cost' => fake()->numberBetween(1, 10),
+            'starting_bid' => $startingBid,
+            'current_bid' => $startingBid,
+            'reserve_price' => $reservePrice,
+            'starts_at' => $startsAt,
+            'ends_at' => $endsAt,
+            'status' => 'active',
         ];
     }
 }
