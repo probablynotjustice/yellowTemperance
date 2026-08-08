bd91e36489dbf43d0eff29fd1bd93017be2c79c2
probablynotjustice <justice.77.da@gmail.com>
2026-08-08 17:18:47 -0500

    ADMIN:CRUD: Made the CAtegories Index Link to the Show Page

--- yellowTemperance/resources/views/admin/categories/show.blade.php
+++ yellowTemperance/resources/views/admin/categories/show.blade.php
@@ -2,45 +2,112 @@
 
 @section('content')
 
-<h1>{{ $category->name }}</h1>
+<admin-layout>
 
-@foreach($category->products as $product)
 
-    <h3>{{ $product->name }}</h3>
+    <div>
+        <a href="{{ route('admin.categories.index') }}">
+            ← Back to Categories
+        </a>
 
-    @forelse($product->auctions as $auction)
+        <a href="{{ route('admin.categories.edit', $category) }}">
+            Edit Category
+        </a>
+    </div>
 
-        <div>
-            <strong>Auction #{{ $auction->id }}</strong><br>
+    <div class="border p-4 rounded mb-4">
 
-            Starting Bid:
-            ${{ $auction->starting_bid }}
+        <h1>{{ $category->name }}</h1>
 
-            <br>
+        @if($category->description)
+            <p>{{ $category->description }}</p>
+        @endif
 
-            Ticket Cost:
-            ${{ $auction->ticket_cost }}
+    </div>
 
-            <br>
 
-            Status:
-            {{ $auction->status }}
+    <h2>Products & Auctions</h2>
 
-            <br>
 
-            <a href="{{ route('admin.auctions.show', $auction) }}">
-                View Auction
-            </a>
+    @forelse($category->products as $product)
+
+        <div class="border p-4 rounded mb-4">
+
+            <h3>
+                <a href="{{ route('admin.products.show', $product) }}">
+                    {{ $product->name }}
+                </a>
+            </h3>
+
+            <p>
+                {{ $product->description }}
+            </p>
+
+            <p>
+                Vendor:
+                {{ $product->vendor->name }}
+            </p>
+
+
+            <h4>Auctions</h4>
+
+            @forelse($product->auctions as $auction)
+
+                <div class="border p-3 rounded mb-2">
+
+                    <h5>
+                        Auction #{{ $auction->id }}
+                    </h5>
+
+                    <p>
+                        Status:
+                        {{ $auction->status }}
+                    </p>
+
+                    <p>
+                        Starting Bid:
+                        ${{ $auction->starting_bid }}
+                    </p>
+
+                    <p>
+                        Ticket Cost:
+                        ${{ $auction->ticket_cost }}
+                    </p>
+
+                    <p>
+                        Starts:
+                        {{ $auction->starts_at }}
+                    </p>
+
+                    <p>
+                        Ends:
+                        {{ $auction->ends_at }}
+                    </p>
+
+                    <a href="{{ route('admin.auctions.show', $auction) }}">
+                        View Auction
+                    </a>
+
+                </div>
+
+            @empty
+
+                <p>
+                    No auctions have been created for this product.
+                </p>
+
+            @endforelse
+
         </div>
-
-        <hr>
 
     @empty
 
-        <p>No auctions for this product.</p>
+        <p>
+            No products have been assigned to this category.
+        </p>
 
     @endforelse
 
-@endforeach
+</admin-layout>
 
 @endsection
